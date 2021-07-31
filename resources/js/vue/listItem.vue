3833c7374b084dba5fcdcc8a73ef1d87e8cfd423
Bilal Asif <bilalasif435@hotmail.com>
2021-08-01 02:35:09 +0500

    setup the entire vue application

---
+++ resources/js/vue/listItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="item">
+    <input
+      type="checkbox"
+      class="complted-checkbox"
+      @change="updateCheck"
+      v-model="item.completed"
+    />
+    <span :class="[item.completed ? 'completed' : '', 'itemText']">{{
+      item.name
+    }}</span>
+    <button @click="remvoeItem" class="trashcan">
+      <font-awesome-icon icon="trash" />
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: "listItem",
+  props: ["item"],
+  methods: {
+    updateCheck() {},
+    remvoeItem() {},
+  },
+};
+</script>
+<style scoped>
+.completed {
+  text-decoration: line-through;
+  color: #999999;
+}
+.itemText {
+  width: 100%;
+  margin-left: 20px;
+  /* color: #ffff; */
+}
+.item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.trashcan {
+  background: #e6e6e6;
+  border: none;
+  color: #ff0000;
+  outline: none;
+  cursor: pointer;
+}
+.complted-checkbox {
+  cursor: pointer;
+}
+</style>
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="(item, index) in items" :key="index">
+      <listItem :item="item" class="item" />
+    </div>
+  </div>
+</template>
+<script>
+import listItem from "./listItem.vue";
+export default {
+  name: "listView",
+  props: ["items"],
+  components: { listItem },
+};
+</script>
+<style >
+.item {
+  background: #e6e6e6;
+  padding: 5px;
+  margin-top: 5px;
+}
+</style>
