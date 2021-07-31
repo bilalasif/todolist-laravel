3fde3733e6eb3cd433eaeb041910d1cd8b83484f
Bilal Asif <bilalasif435@hotmail.com>
2021-08-01 02:34:48 +0500

    removed the boilerplate code from the blade file and also added the right div to hook vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+import Vue from 'vue';
+import App from './vue/app';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash, faUserSecret, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash, faUserSecret, faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+})
